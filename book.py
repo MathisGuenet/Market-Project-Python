7d5abe93ec4f44a91d5894701a460b7f71d1691a
mama2e99 <>
2021-03-10 17:35:53 +0100

    update book class

--- book.py
+++ book.py
@@ -4,11 +4,19 @@ class Order:
         self.price = price
         self.buy = buy
 
+    def newOrderId(self):
+        self.orderId+=1
+        return self.orderId
+
 
     def __str__ (self):
         return "merci mathis il me carry, il aura droit à une chouffe à l'ouverture des bars mais faut pas le dire"
+
+    def __eq__(self, other):
+        return other and self.quantity == other.quantity and self.price == other.price
     
-    def __repr__(self):
+    def __lt__(self, other):  #self < other
+        return self.price < other.price
 
     def quantity(self):
         return self.quantity if self.buy else - self.quantity
@@ -22,45 +30,33 @@ class Book:
         self.name = name
         self.buyList = list()
         self.sellList = list()
-        self.orderId = 0
-
-    def newOrderId(self):
-        self.orderId+=1
-        return self.orderId
-
-    def maxBuy( ):
-    
-
-
-    def minSell( ):
-
-
-
         
-
-    def insertBuy(quantity, price, Id):
+    def insertBuy(self, quantity, price):
         order = Order(quantity, price, True)
         i=0
-        while True : 
-            if price >= sellList[i].price and quantity > 0: 
+        while True :
+            if not self.sellList :
+                self.buyList.append(order)
+            elif price >= self.sellList[i].price and quantity > 0: 
                 #on execute l'ordre
-                if quantity - sellList[i].quantity >=  0 :
-                    quantity = quantity - sellList[i].quantity
-                    sellList.pop(i)
+                if quantity - self.sellList[i].quantity >=  0 :
+                    quantity = quantity - self.sellList[i].quantity
+                    self.sellList.pop(i)
 
-                else if quantity - sellList[i].quantity < 0:
+                elif quantity - sellList[i].quantity < 0:
                     sellList[i].quantity = sellList[i].quantity - quantity
                     quantity = 0
+
+            else :
+                self.buyList.append(order)
             i= i+1
-       if
-                
-     self.buyList.append(order)
-        
 
+    def sortBuyList(self):
+        for i in range(len(self.buyList)):
+            if buyList[i + 1].price < buyList[i].price:
+                a = buyList[i]
+                buyList[i] = buyList[i + 1]
+                buyList[i + 1] = a
 
-
-
-    def insertSell(quantity, price, Id):
-        order = Order(quantity, price, False)
-        self.sellList.append(order)
-
+book = Book("TEST")
+book.insertBuy(10, 10.0)
